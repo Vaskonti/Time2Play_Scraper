6c9a97952265c91157878a57d214d1c246b0517c
Vasil <vortexblast550@gmail.com>
2024-03-16 13:35:50 +0200

    [finale?]

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 echo "Change permissions of the file"
-cp /home/ubuntu/scripts/Time2Play_Scraper/.env /home/ubuntu/scripts/.env
-rm -rf /home/ubuntu/scripts/Time2Play_Scraper
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 echo "Starting server"
-mv /home/ubuntu/scripts/.env /home/ubuntu/scripts/Time2Play_Scraper/.env
